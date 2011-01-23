b2d051e734fbc277d2fee5c21acc2c71719d7fdd
Massive Interactive <register@massive.com.au>
2011-01-24 09:25:33 +1100

    autogen copyright changed year to 2011

--- core/src/massive/munit/ITestResultClient.hx
+++ core/src/massive/munit/ITestResultClient.hx
@@ -1,5 +1,5 @@
 /****
-* Copyright 2010 Massive Interactive. All rights reserved.
+* Copyright 2011 Massive Interactive. All rights reserved.
 * 
 * Redistribution and use in source and binary forms, with or without modification, are
 * permitted provided that the following conditions are met:
@@ -24,7 +24,6 @@
 * The views and conclusions contained in the software and documentation are those of the
 * authors and should not be interpreted as representing official policies, either expressed
 * or implied, of Massive Interactive.
-* 
 ****/
 
 package massive.munit;
