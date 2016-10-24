447414e96c04fd9c96a1be9f5dc1135af7a8445b
slavara <ratdg@ya.ru>
2016-10-24 18:22:25 +0300

    Minor...

--- src/massive/munit/Assert.hx
+++ src/massive/munit/Assert.hx
@@ -1,15 +1,15 @@
 /****
-* Copyright 2013 Massive Interactive. All rights reserved.
+* Copyright 2016 Massive Interactive. All rights reserved.
 * 
 * Redistribution and use in source and binary forms, with or without modification, are
 * permitted provided that the following conditions are met:
 * 
-*	1. Redistributions of source code must retain the above copyright notice, this list of
-*	   conditions and the following disclaimer.
+*    1. Redistributions of source code must retain the above copyright notice, this list of
+*       conditions and the following disclaimer.
 * 
-*	2. Redistributions in binary form must reproduce the above copyright notice, this list
-*	   of conditions and the following disclaimer in the documentation and/or other materials
-*	   provided with the distribution.
+*    2. Redistributions in binary form must reproduce the above copyright notice, this list
+*       of conditions and the following disclaimer in the documentation and/or other materials
+*       provided with the distribution.
 * 
 * THIS SOFTWARE IS PROVIDED BY MASSIVE INTERACTIVE ``AS IS'' AND ANY EXPRESS OR IMPLIED
 * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
@@ -25,6 +25,8 @@
 * authors and should not be interpreted as representing official policies, either expressed
 * or implied, of Massive Interactive.
 ****/
+
+
 
 package massive.munit;
 
