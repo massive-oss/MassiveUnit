b155942b12c8cdbf97783c3459a12ee27859003c
Joshua Granick <jgranick@users.noreply.github.com>
2017-09-14 16:02:05 -0700

    Fix compile with OpenFL development version

--- src/massive/munit/client/HTTPClient.hx
+++ src/massive/munit/client/HTTPClient.hx
@@ -327,7 +327,7 @@ class URLRequest
 	#if flash9
 		function internalOnData(event:flash.events.Event) 
 		{
-			onData(event.target.data);
+			onData(cast (event.target, URLRequest).data);
 		}
 
 		function internalOnError(event:flash.events.Event)
