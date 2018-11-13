31e8d5845c5b8ff1897989ae681694ddd5616ee7
Joshua Granick <jgranick@users.noreply.github.com>
2018-11-12 22:47:25 -0800

    Async examples

--- src/massive/munit/Async.hx
+++ src/massive/munit/Async.hx
@@ -1,11 +1,12 @@
 package massive.munit;
 
+import haxe.Constraints;
 import haxe.PosInfos;
 import massive.munit.Assert;
 
 class Async
 {
-	public static function handler(testCase:Dynamic, callbackFunc:Dynamic, timeout:Int = 1000, timeoutHandler:Dynamic = null, ?info:PosInfos):Dynamic
+	public static function handler(testCase:Dynamic, callbackFunc:Function, timeout:Int = 1000, timeoutHandler:Function = null, ?info:PosInfos):Dynamic
 	{
 		if (TestRunner.activeRunner == null)
 		{
