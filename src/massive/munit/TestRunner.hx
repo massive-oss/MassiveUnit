d0f6e262a309795d1aadb59d48a60f36409c7d80
AlexHaxe <Alexander.Blum@gmail.com>
2020-12-19 12:26:14 +0100

    fixed thread API change in Haxe nightlies

--- src/massive/munit/TestRunner.hx
+++ src/massive/munit/TestRunner.hx
@@ -189,7 +189,11 @@ class TestRunner implements IAsyncDelegateObserver
 
         #if (neko || cpp || java || hl || eval)
 		var self = this;
-		var runThread:Thread = Thread.create(function()
+		var createThread = Thread.create;
+		#if (haxe4 && (haxe >= version("4.2.0-rc.1")))
+		createThread = Thread.createWithEventLoop;
+		#end
+		var runThread:Thread = createThread(function()
 		{
 			self.execute();
 			while (self.running)
